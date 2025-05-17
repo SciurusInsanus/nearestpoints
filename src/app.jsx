92808fe6e22fe506f140626383f9d628a9849151
Glitch (glitch-hello-react) <none>
2025-05-17 07:57:12 +0000

    🏰🕹 Checkpoint ./src/app.jsx:77638385/16379

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,27 @@
+// Updated full code with type switch and correct volunteer message logic
+
 import React, { useState } from "react";
 
 const pickupPoints = [
-  { name: "Мария", address: "Боровское шоссе, д.2к7", coords: [55.661496, 37.415622], isVolunteer: true, phone: "+79639764558", nearestMetro: "Говорово" },
-  { name: "Елена", address: "ул. Кусковская, д.17", coords: [55.739407, 37.777419], isVolunteer: true, phone: "+79032322433", nearestMetro: "Перово" },
-  { name: "Дарья", address: "Варшавское шоссе, д.141к13", coords: [55.587779, 37.603415], isVolunteer: true, phone: "+79104742988", nearestMetro: "Аннино" },
-  { name: "Елена", address: "Болотниковская улица, 10А", coords: [55.657306, 37.609991], isVolunteer: true, phone: "+79169798613", nearestMetro: "Нахимовский проспект" },
-  { name: "Галина", address: "ул. Константина Федина, д 8", coords: [55.805827, 37.794064], isVolunteer: true, phone: "+79169810644", nearestMetro: "Щёлковская" },
-  { name: "Анастасия", address: "2-я Хуторская улица, д. 18к2", coords: [55.806762, 37.575109], isVolunteer: true, phone: "+79262113585", nearestMetro: "Дмитровская" },
-  { name: "Янош", address: "Ленинский проспект, д.93", coords: [55.675245, 37.527920], isVolunteer: true, phone: "+79031637020", nearestMetro: "Новаторская" },
-  { name: "Зоомагазин «Хитрый нос»", address: "Ярославское шоссе 12к2", coords: [55.854517, 37.683060], workingHours: "10:00-22:00", nearestMetro: "Ростокино" },
-  { name: "Dr.Vetson", address: "Балаклавский проспект, д. 9", coords: [55.641122, 37.595501], workingHours: "круглосуточно", nearestMetro: "Чертановская" },
-  { name: "Dr.Hug", address: "ул. Малая Филёвская, д.12к1", coords: [55.738302, 37.472917], workingHours: "круглосуточно", nearestMetro: "Пионерская" },
-  { name: "Dr.Hug", address: "Хорошёвское шоссе, д. 38Дс3", coords: [55.781003, 37.537550], workingHours: "круглосуточно", nearestMetro: "ЦСКА" },
-  { name: "Синица", address: "ул. Маршала Неделина, д.16с5", coords: [55.724403, 37.410654], workingHours: "9:00-21:00", nearestMetro: "Молодежная" },
-  { name: "Пара капибар", address: "ул.Народного ополчения, д.48к1", coords: [55.794032, 37.495078], workingHours: "круглосуточно", nearestMetro: "Октябрьское поле" },
-  { name: "Лама рядом", address: "ул.Гродненская, д.10", coords: [55.718076, 37.433597], workingHours: "круглосуточно", nearestMetro: "Давыдково" },
-  { name: "Наш офис", address: "ул.Коцюбинского, д.4", coords: [55.728591, 37.431235], workingHours: "10:00-18:00 по будним дням", nearestMetro: "Кунцевская" },
+  { name: "Мария", address: "Боровское шоссе, д.2к7", coords: [55.661496, 37.415622], phone: "+79639764558", nearestMetro: "Говорово", type: "volunteer" },
+  { name: "Елена", address: "ул. Кусковская, д.17", coords: [55.739407, 37.777419], phone: "+79032322433", nearestMetro: "Перово", type: "volunteer" },
+  { name: "Дарья", address: "Варшавское шоссе, д.141к13", coords: [55.587779, 37.603415], phone: "+79104742988", nearestMetro: "Аннино", type: "volunteer" },
+  { name: "Елена", address: "Болотниковская улица, 10А", coords: [55.657306, 37.609991], phone: "+79169798613", nearestMetro: "Нахимовский проспект", type: "volunteer" },
+  { name: "Галина", address: "ул. Константина Федина, д 8", coords: [55.805827, 37.794064], phone: "+79169810644", nearestMetro: "Щёлковская", type: "volunteer" },
+  { name: "Анастасия", address: "2-я Хуторская улица, д. 18к2", coords: [55.806762, 37.575109], phone: "+79262113585", nearestMetro: "Дмитровская", type: "volunteer" },
+  { name: "Янош", address: "Ленинский проспект, д.93", coords: [55.675245, 37.527920], phone: "+79031637020", nearestMetro: "Новаторская", type: "volunteer" },
+  { name: "Хитрый нос", address: "Ярославское шоссе 12к2", coords: [55.854517, 37.683060], workingHours: "10:00-22:00. Передать помощь в данный пункт сбора можно в часы работы магазина без каких-либо предварительных договорённостей", nearestMetro: "Ростокино", type: "shop" },
+  { name: "Dr.Vetson", address: "Балаклавский проспект, д. 9", coords: [55.641122, 37.595501], workingHours: "круглосуточно. Передать помощь в данный пункт сбора можно в любое удобное время без каких-либо предварительных договорённостей", nearestMetro: "Чертановская", type: "vet" },
+  { name: "Dr.Hug", address: "ул. Малая Филёвская, д.12к1", coords: [55.738302, 37.472917], workingHours: "круглосуточно. Передать помощь в данный пункт сбора можно в любое удобное время без каких-либо предварительных договорённостей", nearestMetro: "Пионерская", type: "vet" },
+  { name: "Dr.Hug", address: "Хорошёвское шоссе, д. 38Дс3", coords: [55.781003, 37.537550], workingHours: "круглосуточно. Передать помощь в данный пункт сбора можно в любое удобное время без каких-либо предварительных договорённостей", nearestMetro: "ЦСКА", type: "vet" },
+  { name: "Синица", address: "ул. Маршала Неделина, д.16с5", coords: [55.724403, 37.410654], workingHours: "9:00-21:00. Передать помощь в данный пункт сбора можно в часы работы клиники без каких-либо предварительных договорённостей", nearestMetro: "Молодежная", type: "vet" },
+  { name: "Пара капибар", address: "ул.Народного ополчения, д.48к1", coords: [55.794032, 37.495078], workingHours: "круглосуточно. Передать помощь в данный пункт сбора можно в любое удобное время без каких-либо предварительных договорённостей", nearestMetro: "Октябрьское поле", type: "vet" },
+  { name: "Лама рядом", address: "ул.Гродненская, д.10", coords: [55.718076, 37.433597], workingHours: "круглосуточно. Передать помощь в данный пункт сбора можно в любое удобное время без каких-либо предварительных договорённостей", nearestMetro: "Давыдково", type: "vet" },
+  { name: "Наш офис", address: "ул.Коцюбинского, д.4", coords: [55.728591, 37.431235], workingHours: "10:00-18:00 по будним дням. Перед приездом просим вас согласовать время посещения в ответном письме или по телефону офиса: +79850667749", nearestMetro: "Кунцевская", type: "office" },
 ];
 
+const toRad = (val) => (val * Math.PI) / 180;
 const haversineDistance = ([lat1, lon1], [lat2, lon2]) => {
-  const toRad = (val) => (val * Math.PI) / 180;
   const R = 6371;
   const dLat = toRad(lat2 - lat1);
   const dLon = toRad(lon2 - lon1);
@@ -32,9 +34,7 @@ const haversineDistance = ([lat1, lon1], [lat2, lon2]) => {
 
 const fetchCoords = async (stationName) => {
   const apiKey = "74796fe5-c44e-403a-b715-a6e954b3118e";
-  const url = `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${apiKey}&geocode=метро ${encodeURIComponent(
-    stationName
-  )}, Москва`;
+  const url = `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${apiKey}&geocode=метро ${encodeURIComponent(stationName)}, Москва`;
   const res = await fetch(url);
   const data = await res.json();
   const pos = data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
@@ -52,28 +52,11 @@ const NearestPickupPoint = () => {
     setLoading(true);
     try {
       const stationCoords = await fetchCoords(station);
-
-      const distances = pickupPoints.map((point) => ({
-        ...point,
-        distance: haversineDistance(stationCoords, point.coords),
-      }));
-
+      const distances = pickupPoints.map((p) => ({ ...p, distance: haversineDistance(stationCoords, p.coords) }));
       const sorted = distances.sort((a, b) => a.distance - b.distance);
-
-      let nearestPoints;
-      if (sorted[0].distance <= 1.5) {
-        nearestPoints = [sorted[0]];
-      } else {
-        nearestPoints = sorted.slice(0, 2);
-      }
-
-      const formatted = nearestPoints.map((point) => ({
-        ...point,
-        distance: point.distance.toFixed(2),
-      }));
-
-      setResult({ nearestPoints: formatted, stationCoords });
-    } catch (e) {
+      const nearestPoints = sorted[0].distance <= 1.5 ? [sorted[0]] : sorted.slice(0, 2);
+      setResult({ nearestPoints: nearestPoints.map((p) => ({ ...p, distance: p.distance.toFixed(2) })) });
+    } catch {
       alert("Ошибка при геокодировании станции.");
     }
     setLoading(false);
@@ -81,27 +64,32 @@ const NearestPickupPoint = () => {
 
   const generateLetter = () => {
     if (!result) return "";
-
-    let volunteerMessage = "";
     let volunteerCount = 0;
-
     const letterBody = result.nearestPoints
       .map((point) => {
-        if (point.isVolunteer) {
-          volunteerCount++;
-          return `- у нашего волонтёра в районе м. ${point.nearestMetro}, контакт волонтёра, с которым можно обсудить передачу помощи: ${point.phone}, ${point.name}.`;
-        } else {
-          return `- в ветеринарной клинике "${point.name}" по адресу ${point.address}, режим работы: ${point.workingHours}.`;
+        switch (point.type) {
+          case "volunteer":
+            volunteerCount += 1;
+            return `- у нашего волонтёра в районе м. ${point.nearestMetro}, контакт: ${point.phone}, ${point.name}.`;
+          case "vet":
+            return `- в ветеринарной клинике "${point.name}" по адресу ${point.address}, режим работы: ${point.workingHours}.`;
+          case "shop":
+            return `- в зоомагазине "${point.name}" по адресу ${point.address}, режим работы: ${point.workingHours}.`;
+          case "office":
+            return `- в офисе нашего фонда по адресу ${point.address}, часы работы: ${point.workingHours}.`;
+          default:
+            return `- в пункте сбора "${point.name}" по адресу ${point.address}.`;
         }
       })
       .join("\n");
 
+    let volunteerMessage = "";
     if (volunteerCount === 1) {
-      volunteerMessage = `Не затруднит ли вас связаться с волонтёром самостоятельно, чтобы согласовать все детали напрямую? При обращении можете сказать, что контакт вам дали в фонде 'РЭЙ'.`;
+      volunteerMessage = "Не затруднит ли вас связаться с волонтёром самостоятельно, чтобы согласовать все детали напрямую? При обращении можете сказать, что контакт вам дали в фонде 'РЭЙ'.";
     } else if (volunteerCount === 2) {
-      volunteerMessage = `Не затруднит ли вас связаться с выбранным волонтёром самостоятельно, чтобы согласовать все детали напрямую? При обращении можете сказать, что контакт вам дали в фонде 'РЭЙ'.`;
+      volunteerMessage = "Не затруднит ли вас связаться с выбранным волонтёром самостоятельно, чтобы согласовать все детали напрямую? При обращении можете сказать, что контакт вам дали в фонде 'РЭЙ'.";
     } else if (volunteerCount > 2) {
-      volunteerMessage = `Пожалуйста, свяжитесь с одним из волонтёров напрямую. При обращении можете сказать, что контакт вам дали в фонде 'РЭЙ'.`;
+      volunteerMessage = "Пожалуйста, свяжитесь с одним из волонтёров напрямую. При обращении можете сказать, что контакт вам дали в фонде 'РЭЙ'.";
     }
 
     return `${username}, добрый день!
@@ -113,7 +101,6 @@ const NearestPickupPoint = () => {
 Мы будем вам очень признательны, если вы сможете передать их в один из пунктов сбора помощи, расположенных в следующих местах:
 
 ${letterBody}
-
 ${volunteerMessage}
 
 Если передать помощь не получится по какой-либо причине, пожалуйста, напишите нам снова.
@@ -171,3 +158,4 @@ ${volunteerMessage}
 };
 
 export default NearestPickupPoint;
+
